709b0dd4867b2537666b79fc19aecf9b420159c2
Kamil <KamilWodowski@o2.pl>
2023-10-24 14:53:31 +0200

    finished lab 3.1 in php

---
+++ php/lab3/utils.php
@@ -0,0 +1,35 @@
+<?php
+function save(): void{
+    // creating data string
+    $data = "";
+    foreach ($_POST as $key => $value) {
+        if(is_array($value)){
+            $data .= implode(",", $value) . " ";
+        } else {
+            $data .= $value . " ";
+        }
+    }
+    $data .="\n";
+
+    // saving data
+    $file = fopen("data.txt", "a") or die("Cannot open file");
+    fwrite($file, $data) or die("Cannot write to a file");
+    fclose($file);
+}
+
+function show(): void{
+    $orders = file("data.txt");
+    foreach ($orders as $order){
+        echo $order."<br>";
+    }
+}
+
+function showOnly($filter): void{
+    $orders = file("data.txt");
+    foreach ($orders as $order){
+        if(str_contains($order, $filter)){
+            echo $order."<br>";
+        }
+    }
+}
+?>
